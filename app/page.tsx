d80f2225c4298b989d31a652a6d3bc2373f2894c
lowkey3Dfx <Arif.Kesdiren@gmail.com>
2023-03-17 15:00:07 +0100

    Add first iteration styling for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,24 @@ import GetVideo from './transcript/transcript';
 
 export default function Home() {
   return (
-    <main>
-      <p>Save your Favorites</p>
-      <button>Sign up</button>
-      <button>Login</button>
-      <h1>Summarize YouTube Transcripts in Seconds</h1>
-      <p>Paste YouTube video URL you want to summarize and give it a "Go"</p>
-      <GetVideo children={undefined} />
+    <main className={styles.pageMain}>
+      <div className={styles.mainDiv}>
+        <div className={styles.divOne}>
+          <p>Save your Favorites</p>
+          <button>Sign up</button>
+          <button>Login</button>
+        </div>
+        <h1>
+          Summarize YouTube
+          <br /> Transcripts in Seconds
+        </h1>
+        <h2>Paste YouTube URL and and summarize</h2>
+        <div className={styles.divTwo}>
+          <input placeholder="URL..." />
+          <button className={styles.summarizeButton}>Summarize</button>
+          {/* <GetVideo children={undefined} /> */}
+        </div>
+      </div>
     </main>
   );
 }
