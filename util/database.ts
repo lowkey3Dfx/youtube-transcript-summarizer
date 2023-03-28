d2420b74ffc2f17a9580f333d0dbb8f564205c0d
lowkey3Dfx <Arif.Kesdiren@gmail.com>
2023-03-28 19:44:28 +0200

    Add updated database.ts file

--- util/database.ts
+++ util/database.ts
@@ -1,21 +1,21 @@
 import { execa } from 'execa';
 
-export async function getVideoId() {
-  try {
-    const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/video`;
-    const response = await fetch(url);
-    // console.log(response);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    // console.log(data);
-    // returning videoId from response
-    return data.videoId;
-  } catch (error) {
-    console.error('There was a error:', error);
-  }
-}
+// export async function getVideoId() {
+//   try {
+//     const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/video`;
+//     const response = await fetch(url);
+//     // console.log(response);
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     const data = await response.json();
+//     // console.log(data);
+//     // returning videoId from response
+//     return data.videoId;
+//   } catch (error) {
+//     console.error('There was a error:', error);
+//   }
+// }
 
 export async function runPythonScript(videoId: string) {
   const command = ['app/getTranscript/tget.py', videoId];
